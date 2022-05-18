5557afb4b384911bd4f985995184a34efc8d7056
Sri Shatagopam <76266488+srishatagopam@users.noreply.github.com>
2022-05-18 11:31:07 -0700

    Update dataloader.py
    Fixed errors with paths.

--- dataloader.py
+++ dataloader.py
@@ -1,25 +1,28 @@
 import torch
 import glob
 import numpy as np
-from torch.utils.data import Dataset
+from torch.utils.data import Dataset, DataLoader
 from torchvision.io import read_image
 from itertools import islice
 
 class ImageDataset(Dataset):
   def __init__(self, dir):
     self.dir = dir
-    self.gen = glob.iglob(dir)
+    self.gen = glob.iglob(dir + '*')
+    self.data = []
+    for fname in self.gen:
+      self.data.append(fname)
 
   def __len__(self):
-    return sum(1 for _ in self.gen)
+    return len(self.data)
 
   def __getitem__(self, idx):
-    fname = next(islice(self.gen, idx, None))
+    fname = self.data[idx]
     image = read_image(fname)
     label = self.getclass(fname)
     return image, label
 
-  def getclass(fname):
+  def getclass(self, fname):
     classes = {
         (0, 0) : 'chd_male',
         (0, 1) : 'chd_female',
@@ -30,7 +33,8 @@ class ImageDataset(Dataset):
         (3, 0) : 'eld_male',
         (3, 1) : 'eld_female'
     }
-    age, gender, _, _ = fname.split('_')
+    age, gender, _, _ = fname[len(self.dir):].split('_')
+    age, gender = int(age), int(gender)
     agebin = 0
     if 0 <= age <= 18: agebin = 0
     elif 18 < age <= 40: agebin = 1
@@ -38,5 +42,5 @@ class ImageDataset(Dataset):
     else: agebin = 3
 
     categorical = classes[(agebin, gender)]
-    val = classes.values().index(categorical)
+    val = list(classes.values()).index(categorical)
     return np.array([0 if i != val else 1 for i in range(8)])
