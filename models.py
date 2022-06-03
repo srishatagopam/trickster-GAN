1fa8b4d7c07e58da1f1c69dc356a58f933549bea
Sri Shatagopam <76266488+srishatagopam@users.noreply.github.com>
2022-06-03 14:09:45 -0700

    Update models.py

--- models.py
+++ models.py
@@ -1,7 +1,8 @@
 import torch
 import torch.nn as nn
 
-def PGD(model, image, target, device='cuda', n_iter=30, alpha=1e-3, eps=0.6):
+
+def PGD(model, image, target, device='cuda', n_iter=5, alpha=1e-3, eps=0.6):
   crit = nn.CrossEntropyLoss()
   model.eval()
   if not isinstance(target, torch.Tensor): target = torch.Tensor([target]).type(torch.int64)
@@ -18,43 +19,62 @@ def PGD(model, image, target, device='cuda', n_iter=30, alpha=1e-3, eps=0.6):
   return pert
 
 class Generator(nn.Module):
-  def __init__(self, target):
+  def __init__(self):
     super(Generator, self).__init__()
-    self.target = target
     self.encoder = Encoder()
-    self.resblocks = ResBlockTrain()
+    self.resblock1 = ResBlock(64, 3, 1, 1)
+    self.resblock2 = ResBlock(64, 3, 1, 1)
     self.decoder = Decoder()
     
-    def forward(self, x):
-      out = self.encoder(x)
-      N, _, H, W = out.shape
-      target = torch.full_like([N, 1, H, W], self.target)
-      out = torch.concat([out, target], dim=1)
-      out = self.resblocks(out)
-      out = self.decoder(out)
-      return out
+  def forward(self, x):
+    out = self.encoder(x)
+    # N, _, H, W = out.shape
+    # out = torch.flatten(out)
+    # concat = torch.concat([out, target])
+    # out = nn.Linear(concat.shape[0], 8*3*128*128)(concat)
+    # out = out.view(8, 3, 128, 128)
+    out = self.resblock1(out)
+    out = self.resblock2(out)
+    out = self.decoder(out)
+    return out
 
 class Discriminator(nn.Module):
   def __init__(self, channels=3):
     super(Discriminator, self).__init__()
     self.encode1 = EncodeBlock(3, 8, 7, 2, 3)
     self.encode2 = EncodeBlock(8, 16, 3, 2, 1)
-    self.encode3 = EncodeBlock(16, 32, 3, 2, 1)
-    self.conv = nn.Conv2d(32, 64, 3, 2, 1)
+    self.conv = nn.Conv2d(16, 32, 3, 2, 1)
     self.dropout = nn.Dropout(0.2)
-    self.sigmoid = nn.Sigmoid()
+    self.fc1 = nn.Linear(32*16*16, 128)
+    self.fc2 = nn.Linear(128, 8)
+    self.fc3 = nn.Linear(128, 1)
+    self.sig = nn.Sigmoid()
+
+  def forward(self, x):
+    out = self.encode1(x)
+    out = self.encode2(out)
+    out = self.conv(out)
+    out = self.dropout(out)
+    out = torch.flatten(out, 1)
+    out = self.fc1(out)
+    class_prob = self.fc2(out)
+    fake_prob = self.sig(self.fc3(out))
+
+    return fake_prob, class_prob
+
+class Encoder(nn.Module):
+  def __init__(self, channels=3):
+    super(Encoder, self).__init__()
+    self.channels = channels
+    self.encode1 = EncodeBlock(self.channels, 16, 7, 2, 3)
+    self.encode2 = EncodeBlock(16, 32, 3, 2, 1)
+    self.encode3 = EncodeBlock(32, 64, 3, 2, 1)
 
   def forward(self, x):
     out = self.encode1(x)
     out = self.encode2(out)
     out = self.encode3(out)
-    out = self.conv(out)
-    out = self.dropout(out)
-    out = torch.flatten(out)
-    class_prob = nn.Linear(out.shape[0], 8)
-    fake_prob = self.sigmoid(out)
-
-    return fake_prob, class_prob
+    return out
 
 class EncodeBlock(nn.Module):
   def __init__(self, in_channels, out_channels, kernel, stride, padding):
@@ -77,51 +97,31 @@ class EncodeBlock(nn.Module):
     out = self.norm(out)
     return out
 
-class Encoder(nn.Module):
-  def __init__(self, channels=3):
-    super(Encoder, self).__init__()
-    self.channels = channels
-    self.encode1 = EncodeBlock(self.channels, 8, 7, 2, 3)
-    self.encode2 = EncodeBlock(8, 16, 3, 2, 1)
-    self.encode3 = EncodeBlock(16, 32, 3, 2, 1)
-    self.encode4 = EncodeBlock(32, 64, 3, 2, 1)
-
-  def forward(self, x):
-    out = self.encode1(x)
-    out = self.encode2(out)
-    out = self.encode3(out)
-    out = self.encode4(out)
-    return out
-
 class ResBlockTrain(nn.Module):
   def _init__(self, channels=64):
     super(ResBlockTrain, self).__init__()
     self.channels = channels
-    self.resblock1 = ResBlock(self.channels, 3, 1, 1, first=True)
+    self.resblock1 = ResBlock(self.channels, 3, 1, 1)
     self.resblock2 = ResBlock(self.channels, 3, 1, 1)
-    self.resblock3 = ResBlock(self.channels, 3, 1, 1)
-    self.resblock4 = ResBlock(self.channels, 3, 1, 1)
 
   def forward(self, x):
     out = self.resblock1(x)
     out = self.resblock2(out)
-    out = self.resblock3(out)
-    out = self.resblock4(out)
     return out
 
 
 class ResBlock(nn.Module):
-  def __init__(self, channels=64, kernel=3, stride=1, padding=1, first=False):
+  def __init__(self, channels=64, kernel=3, stride=1, padding=1):
     super(ResBlock, self).__init__()
     self.channels = channels
     self.kernel = kernel
     self.stride = stride
     self.padding = padding
 
-    if first: 
-      self.conv1 = nn.Conv2d(self.channels+1, self.channels, 7, self.stride, 3)
-    else:
-      self.conv1 = nn.Conv2d(self.channels, self.channels, self.kernel, self.stride, self.padding)
+    # if first: 
+    #   self.conv1 = nn.Conv2d(self.channels+1, self.channels, 7, self.stride, 3)
+    # else:
+    self.conv1 = nn.Conv2d(self.channels, self.channels, self.kernel, self.stride, self.padding)
     self.conv2 = nn.Conv2d(self.channels, self.channels, self.kernel, self.stride, self.padding)
     self.norm1 = nn.BatchNorm2d(self.channels)
     self.norm2 = nn.BatchNorm2d(self.channels)
@@ -138,6 +138,20 @@ class ResBlock(nn.Module):
     out = self.relu(out)
     out = self.dropout(out)
     out = self.norm2(out)
+    return out
+
+class Decoder(nn.Module):
+  def __init__(self, channels=3):
+    super(Decoder, self).__init__()
+    self.channels = channels
+    self.decode1 = DecodeBlock(64, 32, 3, 2, 0)
+    self.decode2 = DecodeBlock(32, 16, 3, 2, 0)
+    self.decode3 = DecodeBlock(16, self.channels, 3, 2, 0)
+
+  def forward(self, x):
+    out = self.decode1(x)
+    out = self.decode2(out)
+    out = self.decode3(out)
     return out
 
 class DecodeBlock(nn.Module):
@@ -159,20 +173,4 @@ class DecodeBlock(nn.Module):
     out = self.relu(out)
     out = self.dropout(out)
     out = self.norm(out)
-    return out
-
-class Decoder(nn.Module):
-  def __init__(self, channels=3):
-    super(Decoder, self).__init__()
-    self.channels = channels
-    self.decode1 = DecodeBlock(64, 32, 3, 2, 0)
-    self.decode2 = DecodeBlock(32, 16, 3, 2, 0)
-    self.decode3 = DecodeBlock(16, 8, 3, 2, 0)
-    self.decode4 = DecodeBlock(8, self.channels, 3, 2, 0)
-
-  def forward(self, x):
-    out = self.decode1(x)
-    out = self.decode2(out)
-    out = self.decode3(out)
-    out = self.decode4(out)
     return out
